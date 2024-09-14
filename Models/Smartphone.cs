3d3cb43b04fbec49cbe7adcd16d1d03afb5c8e1f
Julio Matias <juliocesark2@hotmail.com>
2024-09-14 02:14:55 -0300

    feat: implementa propriedade e metodos na Smartphone para serem herdados

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,35 @@
+using Newtonsoft.Json;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // TODO: Implementar as propriedades faltantes de aco
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        protected int _memoria;
+        public int Memoria
+        { 
+            get { return _memoria; }
+        }
 
-        public Smartphone(string numero)
+        protected List<App> Apps;
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            if (memoria < 1000) 
+            {
+                throw new ArgumentException("A memória deve ser maior ou igual 1G(1000MG).");
+            }
+            else 
+            {
+                _memoria = memoria;
+            }
         }
 
         public void Ligar()
@@ -22,5 +43,23 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        protected List<App> PegarAplicativos(string dispositivo)
+        {
+            string caminhoArquivo = "database/list_apps.json";
+            
+            string json = File.ReadAllText(caminhoArquivo);
+
+            Dispositivos dispositivos = JsonConvert.DeserializeObject<Dispositivos>(json);
+
+            if (dispositivo == "nokia_apps")
+            {
+                return dispositivos.NokiaApps;
+            }
+            else 
+            {
+                return dispositivos.IphoneApps;
+            }
+        }
     }
 }
